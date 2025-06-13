2da17d2eba8ab2d909ba71e5c6cdfce50b402f58
JEFF1507 <jeffchaves1507@gmail.com>
2025-06-13 16:51:59 -0300

    blade icons instalado

--- laravel/resources/views/movie/show.blade.php
+++ laravel/resources/views/movie/show.blade.php
@@ -1,27 +1,46 @@
 <x-app-layout>
-    <section class="flex items-start gap-8">
+    <section class="flex items-center gap-8">
         @if ($movie['poster_path'])
             <img class="rounded-lg" src="https://image.tmdb.org/t/p/w300{{ $movie['poster_path'] }}" alt="{{ $movie['title'] }}">
         @endif
-        <div class="flex flex-col gap-6 w-full">
+        <div class="flex flex-col gap-4 w-full">
             <h2 class="text-3xl text-zinc-200 font-bold tracking-wide">
                 {{ $movie['title'] }}
             </h2>
-            <div class="flex items-center gap-4 font-bold">
-                <p class="text-zinc-200 ">
-                    {{ date('Y', strtotime($movie['release_date'])) }}
-                </p>
-                <hr class="w-px bg-zinc-200 min-h-4 h-full mx-2">
+            <div class="flex items-center gap-2">
                 @foreach ($movie['genres'] as $genre)
-                    <p class="text-sm text-zinc-200">
+                    <x-chip>
                         {{ $genre['name'] }}
-                    </p>
+                    </x-chip>
                 @endforeach
             </div>
-            <p class="text-sm text-zinc-200">
-                {{ $movie['overview'] }}
-            </p>
-            <div class="w-full">
+            <div class="flex flex-col gap-1">
+                <h3 class="text-base sm:text-xl text-zinc-200 font-semibold">
+                    Sinopse
+                </h3>
+                <p class="text-xs sm:text-sm text-zinc-400">
+                    @if ($movie['overview'])
+                        {{ $movie['overview'] }}
+                    @else
+                        Nenhuma sinopse informada!
+                    @endif
+                </p>
+            </div>
+            <div class="flex items-center gap-8 w-full">
+                <div class="flex items-center gap-2">
+                    <x-heroicon-c-calendar-date-range class="w-5 h-5 text-zinc-200" />
+                    <p class="text-xs sm:text-sm text-zinc-400 font-semibold">
+                        {{ date('Y', strtotime($movie['release_date'])) }}
+                    </p>
+                </div>
+                <div class="flex items-center gap-2">
+                    <x-heroicon-c-clock class="w-5 h-5 text-zinc-200" />
+                    <p class="text-xs sm:text-sm text-zinc-400 font-semibold">
+                        {{ $movie['runtime'] }} min
+                    </p>
+                </div>
+            </div>
+            <div class="w-full mt-4">
                 <x-button>
                     Adicionar a uma lista
                 </x-button>
